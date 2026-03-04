9f2e7a61900895048d3c3e04700625423cca1d02
kaidiya13-hub <3158856W@student.gla.ac.uk>
2026-03-04 22:25:51 +0000

    SC-204: Add mana validation and UI warning when clicking a card

--- app/events/CardClicked.java
+++ app/events/CardClicked.java
@@ -1,31 +1,32 @@
 package events;
 
-
 import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.actor.ActorRef;
+import commands.BasicCommands;
 import structures.GameState;
+import structures.basic.Card;
 
-/**
- * Indicates that the user has clicked an object on the game canvas, in this case a card.
- * The event returns the position in the player's hand the card resides within.
- * 
- * { 
- *   messageType = “cardClicked”
- *   position = <hand index position [1-6]>
- * }
- * 
- * @author Dr. Richard McCreadie
- *
- */
-public class CardClicked implements EventProcessor{
+public class CardClicked implements EventProcessor {
 
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		
-		int handPosition = message.get("position").asInt();
-		
-		
-	}
 
+		int handPosition = message.get("position").asInt();
+
+		// Safety check: prevent clicking an invalid hand position
+		if (handPosition < 1 || handPosition > gameState.humanPlayer.hand.size()) {
+			return;
+		}
+
+		Card clickedCard = gameState.humanPlayer.hand.get(handPosition - 1);
+
+		// [SC-204] Mana check interceptor
+		if (gameState.humanPlayer.getMana() < clickedCard.getManacost()) {
+			BasicCommands.addPlayer1Notification(out, "Not enough mana", 2);
+			return;
+		}
+
+		// (Future logic for playing a card (SC-201) will go down here...)
+	}
 }

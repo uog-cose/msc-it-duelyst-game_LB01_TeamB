041fd38f6575dec7c539ee7a182080f80b8c85c4
Huangzihang0104 <3078595H@student.gla.ac.uk>
2026-03-08 16:03:41 +0000

    Update CardClicked.java

--- app/events/CardClicked.java
+++ app/events/CardClicked.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import akka.actor.ActorRef;
 import structures.GameState;
 import commands.BasicCommands;
-
+import structures.basic.Card;
 /**
  * Indicates that the user has clicked an object on the game canvas, in this case a card.
  * The event returns the position in the player's hand the card resides within.
@@ -24,21 +24,19 @@ public class CardClicked implements EventProcessor{
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 		
-	int handPosition = message.get("position").asInt();
-	
-	Card selectedCard = gameState.humanPlayer.hand.get(handPosition - 1);
-	
-	int cardCost = selectedCard.getManacost();
-	
-	boolean success = gameState.humanPlayer.spendMana(cardCost);
-	
-	if (!success) {
-	    BasicCommands.addPlayer1Notification(out, "Not enough mana!", 2);
-	    return;
-}
-    gameState.humanPlayer.setMana(currentMana - cardCost);	
-	}
+		int handPosition = message.get("position").asInt();
 
+		Card selectedCard = gameState.humanPlayer.hand.get(handPosition - 1);
+		int cardCost = selectedCard.getManacost();
+
+		boolean success = gameState.humanPlayer.spendMana(cardCost);
+
+		if (!success) {
+			BasicCommands.addPlayer1Notification(out, "Not enough mana!", 2);
+			return;
+		}
+	}
 }
+
 
 
